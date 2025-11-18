e7c2d8bb7d6f4e49fbe1fdfad4e2cc2de34006be
Nickspero <nspappas99@gmail.com>
2025-11-18 18:34:19 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,26 +4,33 @@ TWMzH2IFMbdgYjekQ            - public key from EmailJS */
 
 /* function to SEND email*/
 
-function contact(event) {            /*Defines a function named contact that will be called when the form is submitted. It receives the submit event.*/
-    event.preventDefault()           /* by default, submitting a form refreshes the page. use preventDefault to prevent that */
-    const loading = document.querySelector('.modal__overlay--loading')  /*loading = .modal__overlay--loading*/
-    const success = document.querySelector('.modal__overlay--success')  /*success = .modal__overlay--success*/
-    loading.classList.add('modal__overlay--visible');                   /* shows the LOADING OVERLAY by adding a class (VISIBLE, z-index:1)*/      
-    emailjs                    /* in JavaScript library, connects you to emailJS service */
-        .sendForm(             /* method that belongs to emailjs. Ex: emailjs.sendForm(serviceID, templateID, formElement, publicKey), returns a PROMISE */
-            'service_lr8yc0a',    /* service ID from EmailJS */
-            'template_1d9uzy8',   /* template ID from EmailJS */
-            event.target,           /* event.target is just “the thing that triggered the event.” Since this function is attached to the form’s onsubmit, the target of the event is the form itself.*/
-            'TWMzH2IFMbdgYjekQ'   /* public key from EmailJS */
-        ).then(() => {                  /* if email sends successfully*/
-            loading.classList.remove('modal__overlay--visible')  /* hide LOADING OVERLAY by removing VISIBLE class */
-            success.classList.add('modal__overlay--visible')      /*display SUCCESS OVERLAY by adding it to classlist*/
+function contact(event) {            
+    event.preventDefault()           
+    const loading = document.querySelector('.modal__overlay--loading') 
+    const success = document.querySelector('.modal__overlay--success')  
+    loading.classList.add('modal__overlay--visible');                      
+    emailjs                    
+        .sendForm(             
+            'service_lr8yc0a',    
+            'template_1d9uzy8',   
+            event.target,           
+            'TWMzH2IFMbdgYjekQ'   
+        ).then(() => {                 
+            loading.classList.remove('modal__overlay--visible') 
+            success.classList.add('modal__overlay--visible')      
         })
-        .catch(() => {loading.classList.remove('modal__overlay--visible')  /* if email doesnt send for whatever reason, this error shows up*/
+        .catch(() => {loading.classList.remove('modal__overlay--visible')  
             .catch(err => console.log(err))
             alert("Email service is temporarily down. Please contact me at nspappas99@gmail.com")
         })
 }
+
+
+document.querySelectorAll(".project__wrapper").forEach(project => {
+  project.addEventListener('click', ()=>{
+    project.classList.toggle('active')
+  })
+})
 
 
 /* function to toggle Modal on and off*/
@@ -31,7 +38,7 @@ function contact(event) {            /*Defines a function named contact that wil
 let isModalOpen = false
 function toggleModal() {
     console.log(isModalOpen)
-    if (isModalOpen) {         /*IF isModalOpen is TRUE*/
+    if (isModalOpen) {        
         isModalOpen = false
         return document.body.classList.remove('modal--open')
     }
